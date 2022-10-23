4fa110f872b889d5ec8b9ddda5e87193b084472a
Brian Zalewski <brian@megabyte.space>
2022-10-23 05:40:57 +0000

    Update local/codeclimate-format.py

--- local/codeclimate-format.py
+++ local/codeclimate-format.py
@@ -2,9 +2,9 @@ import json
 import sys
 
 with open(sys.argv[1]) as json_file:
-    data = json.load(json_file)
-    
-    for analysis in data:
+    json_data = json.load(json_file)
+
+    for analysis in json_data:
         analysis['categories'] = ['Bug Risk']
         if 'end' not in analysis['location']['lines']:
             analysis['location']['lines']['end'] = analysis['location']['lines']['begin']
